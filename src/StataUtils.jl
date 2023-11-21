25f4f5376a4dd88446f9c4bd4a8340f11866c235
Erik Loualiche <eloualic@umn.edu>
2023-11-21 10:03:13 -0600

    First commit of prototypes

--- src/StataUtils.jl
+++ src/StataUtils.jl
@@ -41,12 +41,18 @@ This was forked from TexTables.jl
 
 # Throws
 - `NotFoundError`: I guess we could throw an error if `val` isn't found.
+
+# TO DO
+allow user to specify order of columns (reorder = false flag)
 """
-function tabulate(df::AbstractDataFrame, 
-    cols::Union{Symbol, Array{Symbol}})
+function tabulate(df::AbstractDataFrame, cols::Union{Symbol, Array{Symbol}};
+    reorder_cols=true)
+
+    N_COLS = size(cols,1)
 
 # debug
     # cols = :island
+    # cols = [:island, :species]
     # df = dropmissing(DataFrame(PalmerPenguins.load()))
 
     if typeof(cols) <: Array{Symbol}
@@ -54,30 +60,28 @@ function tabulate(df::AbstractDataFrame,
     end
 
     # Count the number of observations by `columns`
-    tab = combine(groupby(df, cols), cols => length => :_N)
-
-    # Construct a Frequency Column
-    sort!(tab, cols)
-    vals  = Symbol.(tab[!,cols])
-    freq  = tab[!,:_N]
-    pct   = freq/sum(freq)*100
-    cum   = cumsum(pct)
-
-    # Construct Table 
-    df_out = DataFrame(vals=vals, freq=freq, pct=pct, cum=cum)
+    # tab = combine(groupby(df, cols), cols .=> length)
+    df_out = combine(groupby(df, cols), nrow => :freq, proprow =>:pct)
+    if reorder_cols
+        sort!(df_out, cols)                          # order before we build cumulative
+    end
+    transform!(df_out, :pct => cumsum => :cum)   # Construct a Frequency Column
 
     pretty_table(df_out;
         hlines = [1],
-        vlines = [1],
-        alignment = [:l, :c, :c, :c],
-        cell_alignment     = Dict((1, 1) => :l, 
-            (2, 1) => :l, (3, 1) => :l, (4, 1) => :l),
-        header = [string(cols); "Freq."; "Percent"; "Cum"],
+        vlines = [N_COLS],
+        alignment = vcat(repeat([:l], N_COLS), :c, :c, :c),
+        cell_alignment = reduce(push!, 
+            map(i -> (i,1)=>:l, 1:N_COLS+3), 
+            init=Dict{Tuple{Int64, Int64}, Symbol}()),
+        header = [string.(cols); "Freq."; "Percent"; "Cum"],
         formatters = (ft_printf("%.2f", 3), ft_printf("%.2f", 4)),
         highlighters = (
             hl_col(1, crayon"cyan bold"),
-            hl_custom_gradient(cols=3, colorscheme=:Oranges_9),
-            hl_custom_gradient(cols=4, colorscheme=:BuGn_6)
+            hl_col(2, crayon"cyan bold"),
+            hl_custom_gradient(cols=(N_COLS+1), colorscheme=:Oranges_9, scale=maximum(df_out.freq)),
+            hl_custom_gradient(cols=(N_COLS+2), colorscheme=:Greens_9),
+            hl_custom_gradient(cols=(N_COLS+3), colorscheme=:Greens_9)            
         ),
         border_crayon = crayon"bold yellow", 
         header_crayon = crayon"bold light_green",
@@ -91,13 +95,13 @@ end
 # ------------------------------------------------------------------------------------------
 function hl_custom_gradient(; 
     cols::Int=0, 
-    colorscheme::Symbol=:Oranges_9) 
+    colorscheme::Symbol=:Oranges_9,
+    scale::Int=1) 
 
     Highlighter(
         (data, i, j) -> j==cols,
         (h, data, i, j) -> begin
-            # color = get(colorschemes[colorscheme], data[i, j], (0, 100))
-            color = get(colorschemes[colorscheme], data[i, j], (0, 100))
+            color = get(colorschemes[colorscheme], data[i, j], (0, scale))
              return Crayon(foreground = (round(Int, color.r * 255),
                                          round(Int, color.g * 255),
                                          round(Int, color.b * 255)))
