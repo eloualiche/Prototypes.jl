7df99edf125eaec05d44bc8021912f3be4ae14f8
Erik Loualiche <eloualic@umn.edu>
2024-01-02 12:45:45 -0600

    Dates in project.toml

--- src/Prototypes.jl
+++ src/Prototypes.jl
@@ -5,7 +5,7 @@ import ColorSchemes: get, colorschemes
 import Crayons: @crayon_str
 import DataFrames: AbstractDataFrame, DataFrame, groupby, combine, nrow, proprow, transform!
 import Dates: Dates.format, Dates.now
-import Logging: global_logger, Logging.Debug, Logging.Info, Logging.Warn
+import Logging: global_logger, Logging, Logging.Debug, Logging.Info, Logging.Warn
 import LoggingExtras: ConsoleLogger, EarlyFilteredLogger, FileLogger, MinLevelLogger, TeeLogger, 
     TransformerLogger
 import PrettyTables: Crayon, ft_printf, get, Highlighter, hl_col, pretty_table
