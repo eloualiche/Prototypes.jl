78d29c44cde578f21da5fd34be0ba9f4c1f0a70e
Erik Loualiche <eloualic@umn.edu>
2024-01-02 12:35:44 -0600

    fix logging dependencies

--- src/Prototypes.jl
+++ src/Prototypes.jl
@@ -4,7 +4,7 @@ module Prototypes
 import ColorSchemes: get, colorschemes
 import Crayons: @crayon_str
 import DataFrames: AbstractDataFrame, DataFrame, groupby, combine, nrow, proprow, transform!
-import Logging
+import Logging: global_logger
 import LoggingExtras: ConsoleLogger, EarlyFilteredLogger, FileLogger, MinLevelLogger, TeeLogger, 
     TransformerLogger
 import PrettyTables: Crayon, ft_printf, get, Highlighter, hl_col, pretty_table
