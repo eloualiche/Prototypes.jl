b1b51aca4dcc7ba17192bf3d3d67be641ad31699
Erik Loualiche <eloualic@umn.edu>
2024-01-05 19:36:57 -0600

    Change test to get df as output

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@ using DataFrames
     cols = :island
     col_length = combine(groupby(df, cols), cols .=> length => :_N)
     sort!(col_length, cols)
-    col_tab = tabulate(df, :island);
+    col_tab = tabulate(df, :island; out=:df);
     sort!(col_tab, cols)
 
     @test col_length._N == col_tab.freq
