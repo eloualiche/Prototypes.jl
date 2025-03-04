3bd79eb7acd7bc567b06e45ac9147ba9ed5310cb
Documenter.jl <documenter@juliadocs.github.io>
2025-03-04 03:21:36 +0000

    build based on 3cb513a

--- dev/assets/chunks/p3.DDfRHJ_u.js
+++
@@ -1,1 +0,0 @@
-const s="/Prototypes.jl/dev/assets/p1.BaPhDzAr.svg",t="/Prototypes.jl/dev/assets/p2.BNqEhU6F.svg",o="/Prototypes.jl/dev/assets/p3.dwJ3o4-g.svg";export{s as _,t as a,o as b};
---
+++ dev/assets/chunks/p3.Dq4_ozVa.js
@@ -0,0 +1,1 @@
+const s="/Prototypes.jl/dev/assets/p1.Oz-yowqS.svg",t="/Prototypes.jl/dev/assets/p2.DtxzHqUI.svg",o="/Prototypes.jl/dev/assets/p3.Dlkao3iB.svg";export{s as _,t as a,o as b};
