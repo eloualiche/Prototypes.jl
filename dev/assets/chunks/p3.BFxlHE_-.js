b8e37a1fc36da2b4f36669d2702bc16a7ed893dd
Documenter.jl <documenter@juliadocs.github.io>
2025-05-10 21:15:40 +0000

    build based on 3063320

--- dev/assets/chunks/p3.BFxlHE_-.js
+++
@@ -1,1 +0,0 @@
-const s="/Prototypes.jl/dev/assets/p1.Slh9sr0r.svg",t="/Prototypes.jl/dev/assets/p2.DgzJqat8.svg",o="/Prototypes.jl/dev/assets/p3.C6EFR-R3.svg";export{s as _,t as a,o as b};
---
+++ dev/assets/chunks/p3.DGTGtZi1.js
@@ -0,0 +1,1 @@
+const s="/Prototypes.jl/dev/assets/p1.DbCfoSfa.svg",t="/Prototypes.jl/dev/assets/p2.BdXjKviw.svg",o="/Prototypes.jl/dev/assets/p3.BGXe_svP.svg";export{s as _,t as a,o as b};
